4ce495f59364099ed16322242f1b299ba31c44e2
Jeú Chaves <jeuchaveslima@gmail.com>
2024-07-25 21:08:59 -0400

    feat: add express-session to store the profile selected by the user in a session

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 
+import { sessionConfig } from './shared/services';
 import './shared/services/TranslationsYup';
 import { router } from './routes';
 
+declare module 'express-session' {
+    interface SessionData {
+        perfilId: number;
+    }
+}
 const server = express();
 
+server.use(sessionConfig);
 server.use(express.json());
-
 server.use(router);
 
 export { server };
