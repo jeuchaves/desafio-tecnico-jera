9005d2c0178baf9d06c3f8b0e0ca6a2b1adc5ec9
Jeú Chaves <jeuchaveslima@gmail.com>
2024-07-27 19:57:45 -0400

    hotfix: fixes problem of total count being all users in the list profiles route

--- src/server/controllers/perfis/GetByUid.ts
+++ src/server/controllers/perfis/GetByUid.ts
@@ -5,8 +5,9 @@ import { PerfisProvider } from '../../database/providers';
 
 export const getByUid = async (req: Request, res: Response) => {
     const uid = Number(req.headers.idUsuario);
+
     const result = await PerfisProvider.getByUid(uid);
-    const count = await PerfisProvider.count();
+    const count = await PerfisProvider.count(uid);
 
     if (result instanceof Error) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
